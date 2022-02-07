59af60a870047857de586aa6894907aa83b9a38b
zsoltfehervari <kulioner@protonmail.com>
2022-02-07 20:05:43 +0100

    mobile

--- assets/js/three.js
+++ assets/js/three.js
@@ -1,4 +1,6 @@
-let scene,camera,renderer,controls,sat_model,point_light;
+let scene,camera,renderer,controls,mouse,marker,sat_model,point_light;
+
+let changeSatelliteModel,drawOrbits;
 
 let clock = new THREE.Clock();
 
@@ -13,109 +15,117 @@ import { GLTFLoader } from 'https://cdn.skypack.dev/pin/three@v0.131.3-QQa34rwf1
 import { DRACOLoader } from 'https://cdn.skypack.dev/pin/three@v0.131.3-QQa34rwf1xM5cawaQLl8/mode=imports,min/unoptimized/examples/jsm/loaders/DRACOLoader.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/pin/three@v0.131.3-QQa34rwf1xM5cawaQLl8/mode=imports,min/unoptimized/examples/jsm/controls/OrbitControls.js';
 
- scene = new THREE.Scene();
+
+function init(){
+
+    scene = new THREE.Scene();
  
- camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    10000
-);
+    camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        10000
+    );
 
- 
-renderer = new THREE.WebGLRenderer({antialias:true,alpha: false});
-renderer.setSize(window.innerWidth,window.innerHeight);
+    renderer = new THREE.WebGLRenderer({antialias:true,alpha: false});
+    renderer.setSize(window.innerWidth,window.innerHeight);
 
-renderer.domElement.addEventListener("click", onclick, true);
+    renderer.domElement.addEventListener("click", onclick, true);
 
-let mouse = new THREE.Vector2();
+    mouse = new THREE.Vector2();
 
-function onclick(){
+    document.getElementById('three_map').appendChild(renderer.domElement);
 
-    raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObjects(scene.children, true); //array
+    function onResize(){
+        renderer.domElement.style.width = window.innerWidth + "px";
+        renderer.domElement.style.height = window.innerHeight + "px";
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth,window.innerHeight);
+    
+    }
+    window.addEventListener("resize",onResize);
+    onResize();
 
-    if (intersects.length > 0) {
+    function onclick(){
+
+        raycaster.setFromCamera(mouse, camera);
+        var intersects = raycaster.intersectObjects(scene.children, true); //array
+    
+        if (intersects.length > 0) {
+            
+            //console.log(intersects[0].object.name);
+            controls.target =  intersects[0].object.getWorldPosition(new THREE.Vector3());
+    
+        }
+    
+    }
+    const Cubeloader = new THREE.CubeTextureLoader();
+    const texture = Cubeloader.load([
+            
+                'assets/images/skybox/space/space_lf.png',
+                'assets/images/skybox/space/space_rt.png',
+                'assets/images/skybox/space/space_dn.png',
+                'assets/images/skybox/space/space_up.png',
+                'assets/images/skybox/space/space_ft.png',
+                'assets/images/skybox/space/space_bk.png'
         
-        console.log(intersects[0].object.name);
-        controls.target =  intersects[0].object.getWorldPosition(new THREE.Vector3());
+            ]);
 
+            
+
+    scene.background = texture;
+
+    const GLTFloader = new GLTFLoader();
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( 'https://cdn.skypack.dev/pin/three@v0.131.3-QQa34rwf1xM5cawaQLl8/mode=imports,min/unoptimized/examples/jsm/libs/draco/');
+    GLTFloader.setDRACOLoader( dracoLoader );
+    
+
+    controls = new OrbitControls(camera,renderer.domElement);
+
+    controls.minDistance = 70;
+    controls.maxDistance = 300;
+
+    controls.enableDamping= true;
+    controls.enableKeys = false;
+    controls.enablePan = false;
+
+    /*
+    controls.minAzimuthAngle = 6;
+    controls.maxAzimuthAngle = 7;
+    controls.maxPolarAngle = 2.5;
+    controls.minPolarAngle = 2.2;
+
+    controls.enableZoom = false;
+    controls.enableDamping= true;
+    controls.enableKeys = false;
+    controls.enablePan = false;
+    controls.enabled = true;
+
+
+    */
+
+    
+
+    function randomStarPosition(){
+        return Math.floor((Math.random() * ( 300 - -300 + 1 )) - 250) ;
     }
 
-}
-
-document.getElementById('three_map').appendChild(renderer.domElement);
-
-onResize();
+    for(let i =0; i< 50; i++){
+        let  star = new THREE.Mesh(
+            new THREE.SphereGeometry(0.5, 32, 32),
+            new THREE.MeshBasicMaterial({color:0xffffff}));
+        
+            star.position.x = randomStarPosition();
+            star.position.y = randomStarPosition();
+            star.position.z = randomStarPosition();
     
-
-const Cubeloader = new THREE.CubeTextureLoader();
-        const texture = Cubeloader.load([
-           
-            'assets/images/skybox/space/space_lf.png',
-            'assets/images/skybox/space/space_rt.png',
-            'assets/images/skybox/space/space_dn.png',
-            'assets/images/skybox/space/space_up.png',
-            'assets/images/skybox/space/space_ft.png',
-            'assets/images/skybox/space/space_bk.png'
-    
-        ]);
-
-scene.background = texture;
-
-const GLTFloader = new GLTFLoader();
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( 'https://cdn.skypack.dev/pin/three@v0.131.3-QQa34rwf1xM5cawaQLl8/mode=imports,min/unoptimized/examples/jsm/libs/draco/');
-GLTFloader.setDRACOLoader( dracoLoader );
-
-
-controls = new OrbitControls(camera,renderer.domElement);
-
-controls.minDistance = 70;
-controls.maxDistance = 300;
-
-controls.enableDamping= true;
-controls.enableKeys = false;
-controls.enablePan = false;
-
-/*
-controls.minAzimuthAngle = 6;
-controls.maxAzimuthAngle = 7;
-controls.maxPolarAngle = 2.5;
-controls.minPolarAngle = 2.2;
-
-controls.enableZoom = false;
-controls.enableDamping= true;
-controls.enableKeys = false;
-controls.enablePan = false;
-controls.enabled = true;
-
-
-*/
-
-
-
-function randomStarPosition(){
-
-    return Math.floor((Math.random() * ( 300 - -300 + 1 )) - 250) ;
-}
-
-for(let i =0; i< 50; i++){
-    let  star = new THREE.Mesh(
-        new THREE.SphereGeometry(0.5, 32, 32),
-        new THREE.MeshBasicMaterial({color:0xffffff}));
-    
-        star.position.x = randomStarPosition();
-        star.position.y = randomStarPosition();
-        star.position.z = randomStarPosition();
-
-        scene.add(star);
-}
-
-
+            scene.add(star);
+    }
 
     var obj = new THREE.Object3D();
-    var marker = new THREE.Object3D();
+    marker = new THREE.Object3D();
 
     marker.name = 'marker';
 
@@ -154,9 +164,9 @@ for(let i =0; i< 50; i++){
         
         //controls.target =  gltf.scene.getWorldPosition(new THREE.Vector3());
 
-    })    
+    });
 
-    function changeSatelliteModel(src){
+    changeSatelliteModel = (src)=>{
 
         marker.remove(sat_model.scene);
 
@@ -173,12 +183,9 @@ for(let i =0; i< 50; i++){
                 marker.add(sat_model.scene);
         });
 
-       
-
     }
 
-
-        
+    
 
     /*
     var pointer = new THREE.Mesh(
@@ -217,28 +224,37 @@ for(let i =0; i< 50; i++){
 
 
 
-camera.position.z += 200;
+    camera.position.z += 200;
+
+        
+
+    drawOrbits = ()=>{
+
+        let points = [];
+        
+        let startPoint = new THREE.Vector3(window.sat_orbits[1][0],window.sat_orbits[1][1]);
+        let endPoint = window.sat_orbits[1][window.sat_orbits[1].lenth - 1];
+        
+        var line = new THREE.Line( 
+            new THREE.BufferGeometry().setFromPoints( points ), 
+            new THREE.LineBasicMaterial( { color: 0xf5b905 } ) );
+
+
+            //console.log(line);
+
+            //marker.add(line);
+            
+    }
+
+
+    animate();
 
 
 
-function drawOrbits(){
-    let points = [];
-    
-
-    let startPoint = new THREE.Vector3(window.sat_orbits[1][0],window.sat_orbits[1][1]);
-    let endPoint = window.sat_orbits[1][window.sat_orbits[1].lenth - 1];
-    
-    var line = new THREE.Line( 
-        new THREE.BufferGeometry().setFromPoints( points ), 
-        new THREE.LineBasicMaterial( { color: 0xf5b905 } ) );
-
-
-        console.log(line);
-
-        marker.add();
-           
 }
+ 
 
+ 
 
 
 function animate(){
@@ -259,9 +275,9 @@ function animate(){
     if(window.sat_cords != undefined){
 
         marker.quaternion.setFromEuler(
-            new THREE.Euler(window.sat_cords.lat * rad,0, window.sat_cords.lng * rad,"XYZ"));
+            new THREE.Euler(window.sat_cords.lat * Math.PI / 180,0, window.sat_cords.lng * Math.PI / 180,"XYZ"));
 
-            drawOrbits();
+            //drawOrbits();
 
     }
 
@@ -272,23 +288,9 @@ function animate(){
 
     //console.log(controls.getDistance());
     
-   
-  
 }
 
-function onResize(){
 
-    renderer.domElement.style.width = window.innerWidth + "px";
-    renderer.domElement.style.height = window.innerHeight + "px";
+init();
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth,window.innerHeight);
-
-
-}
-window.addEventListener("resize",onResize);
-
-
-animate();
 
